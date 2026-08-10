3b8f5adc6b962e6cd4655e282d704b8882ac22a2
Izaya-Letselane <139796837+Izaya-Letselane@users.noreply.github.com>
2026-08-10 15:06:41 +0200

    completed projec2t

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,13 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000
 const __dirname = path.resolve()
-app.use(cors({
+if (process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
+}
+
 app.use(express.json({limit:"10mb"}))
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser())
@@ -22,6 +25,15 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/notes", notesRoutes)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../frontend", "dist", "index.html")
+    );
+  });
+}
 
 app.listen(port, ()=>{
     console.log(`Server is running on  http://localhost:${port}`)
